d3f9a5e6e2ce92b9f5a2f81c89fb4d727cfc1751
Ivan <125755207+kurilove@users.noreply.github.com>
2024-10-07 11:33:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import asyncio
-from pyrogram import Client
+import time
 
+from pyrogram import Client
+from rich import console
 
 api_id = 10999414
 api_hash = "c25eb4e3283f5fad6f0397e0463b4eef"
@@ -8,33 +10,36 @@ channel_id = -1001761095736
 mes_id = 357
 message = "тест"
 from_chat = '@spamtext2'
-#залупа
 
-async def main():
-    async with Client("my_account", api_id, api_hash) as app:
-        async for users in app.get_chat_join_requests(chat_id=channel_id):
-            print(users.user.first_name, users.user.last_name)
-            mes_count = await app.get_chat_history_count(users.user.id)
-            print(mes_count)
-            if mes_count == 0:
-                await app.copy_message(users.user.id, message_id=mes_id, from_chat_id=from_chat)
-                await asyncio.sleep(15)
-                await app.approve_chat_join_request(chat_id=channel_id, user_id=users.user.id)
-                print(f"{users.user.first_name}Принят в канал, сообщение отправлено ")
-                await asyncio.sleep(5)
-            else:
-                await asyncio.sleep(15)
-                await app.approve_chat_join_request(chat_id=channel_id, user_id=users.user.id)
-                print(f"{users.user.first_name}Принят в канал, диалог уже есть ")
-                await asyncio.sleep(5)
-                
-        # async for dialog in app.get_dialogs():
-        #     print(dialog.chat.first_name or dialog.chat.title, dialog.chat.id)
-        
-        # async for dialog in app.get_chat_history(from_chat):
-        #     print(dialog)
+# залупа
+while True:
+    async def main():
+        async with Client("my_account", api_id, api_hash) as app:
+            async for users in app.get_chat_join_requests(chat_id=channel_id):
+                print(users.user.first_name, users.user.last_name)
+                mes_count = await app.get_chat_history_count(users.user.id)
+                print(mes_count)
+                if mes_count == 0:
+                    await app.copy_message(users.user.id, message_id=mes_id, from_chat_id=from_chat)
+                    await asyncio.sleep(15)
+                    await app.approve_chat_join_request(chat_id=channel_id, user_id=users.user.id)
+                    print(f"{users.user.first_name}Принят в канал, сообщение отправлено ")
+                    await asyncio.sleep(5)
+                else:
+                    await asyncio.sleep(15)
+                    await app.approve_chat_join_request(chat_id=channel_id, user_id=users.user.id)
+                    print(f"{users.user.first_name}Принят в канал, диалог уже есть ")
+                    await asyncio.sleep(5)
 
-        # await app.copy_message(chat_id="shalto_balto", from_chat_id="from_chat", message_id=mes_id)
+            # async for dialog in app.get_dialogs():
+            #     print(dialog.chat.first_name or dialog.chat.title, dialog.chat.id)
+
+            # async for dialog in app.get_chat_history(from_chat):
+            #     print(dialog)
+
+            # await app.copy_message(chat_id="shalto_balto", from_chat_id="from_chat", message_id=mes_id)
 
 
-asyncio.run(main())
+    asyncio.run(main())
+    print("Курс приема окончен, ждем 1час")
+    time.sleep(3600)
